3f0a8ba43b3458ab565a6242ab93a84e9735d4b9
webdev0728 <webdev1226@outlook.com>
2020-05-01 20:19:31 +0900

    fix_1

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
+ 
+import '../article/style.scss'
 
-const Article = props => (
-  <Link to={`/blog/${props.slug}`} className="c-article">
-    <img alt={props.data.image.alt} src={props.data.image.url} />
-    <h2>{props.data.title.text}</h2>
+const Article = () => (
+  <Link to="/" className="c-article">
+    <img alt="" src="https://source.unsplash.com/random/800x600" />
   </Link>
 )
 
